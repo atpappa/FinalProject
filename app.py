2d224e40929c081f8458bf7bc1f2167828d4c7ee
Jake Geiser <jgeiser@uncc.edu>
2020-11-21 12:30:08 -0500

    added flask app

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, request
+# import predict_model
+import time
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # Return template and data
+    return render_template("index.html")
+    
+
+@app.route("/jockey")
+def misery():
+
+    return render_template("j&t.html")
+    
+
+# @app.route("/predict")
+# def stocks():
+#     return render_template("stockSearch.html")
+
+# @app.route("/predict", methods=['POST'])
+# def predict_input():
+#     in1 = request.form['odds']
+#     in2 = request.form['bankroll']
+
+@app.route("/home")
+def mainpage():
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
